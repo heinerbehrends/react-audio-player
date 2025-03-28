f20507c6f283cf7c96f1106634827bf58ae1fbdc
Heiner Behrends <heinerbehrends@hotmail.com>
2025-03-28 09:16:49 +0100

    Initialize time display with the correct remaining value

--- src/Player/useTimeDisplay.ts
+++ src/Player/useTimeDisplay.ts
@@ -5,25 +5,27 @@ export function useTimeDisplay() {
   const {
     audioElementRef: { current: audioElement },
   } = useContext(AudioContext);
+  const duration = audioElement?.duration ?? 0;
   const [displayTime, setDisplayTime] = useState({
     elapsed: 0,
-    remaining: 0,
+    remaining: duration,
   });
-
   const updateTime = useCallback(() => {
     setDisplayTime({
       elapsed: Math.floor(audioElement?.currentTime ?? 0),
-      remaining: Math.floor(
-        (audioElement?.duration ?? 0) - (audioElement?.currentTime ?? 0)
-      ),
+      remaining: Math.floor(duration - (audioElement?.currentTime ?? 0)),
     });
-  }, [audioElement]);
+  }, [audioElement, duration]);
 
   useEffect(() => {
     if (!audioElement) return;
+    setDisplayTime({
+      elapsed: Math.floor(audioElement?.currentTime ?? 0),
+      remaining: Math.floor(duration - (audioElement?.currentTime ?? 0)),
+    });
     audioElement.addEventListener("timeupdate", updateTime);
     return () => audioElement.removeEventListener("timeupdate", updateTime);
-  }, [audioElement, updateTime]);
+  }, [audioElement, updateTime, duration]);
 
   useEffect(() => {
     if (!audioElement) return;
