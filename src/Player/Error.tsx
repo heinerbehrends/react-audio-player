1a7f84a8de7856cc5696fd0ef97471fa2833220e
Heiner Behrends <heinerbehrends@hotmail.com>
2025-04-12 15:46:26 +0200

    Improve Error component a11y

--- src/Player/Error.tsx
+++ src/Player/Error.tsx
@@ -1,10 +1,19 @@
 import { useContext } from "react";
 import { PlayerContext } from "./PlayerContext";
 
-export function Error({ children }: { children: React.ReactNode }) {
+interface ErrorProps {
+  children: React.ReactNode;
+}
+
+export function Error({ children }: ErrorProps) {
   const { playerState: player } = useContext(PlayerContext);
   if (player === "error") {
-    return <div>{children}</div>;
+    return (
+      <div role="alert" aria-live="assertive" className="audio-player-error">
+        <div aria-hidden="true">{children}</div>
+        <span className="sr-only">There was an error loading the audio</span>
+      </div>
+    );
   }
   return null;
 }
